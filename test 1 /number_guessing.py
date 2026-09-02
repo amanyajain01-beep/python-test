356ff245443495972f27831926e6936d32ab6ecc
amanyajain01-beep <amanya.jain.01@gmail.com>
2026-09-02 18:08:10 +0530

    intial commit

---
+++ test 1 /number_guessing.py
@@ -0,0 +1,62 @@
+print("== GUESSING GAME == ") 
+
+secret_num = 27
+
+guess = int(input("Guess the secret number : "))
+if guess == secret_num : 
+    print ("GOOD JOB THE NUMEBER WAS 27") 
+if guess > secret_num : 
+    print("lower") 
+else: 
+    print("higher") 
+
+    if not guess  == secret_num : 
+        print ("try again")
+        guess = int(input("Guess again : ")) 
+    if guess == secret_num : 
+          print ("GOOD JOB THE NUMEBER WAS 27") 
+if guess > secret_num : 
+    print("lower") 
+else: 
+    print("higher") 
+
+    if not guess  == secret_num : 
+        print ("try again")
+        guess = int(input("Guess again : ")) 
+    if guess == secret_num : 
+            print ("GOOD JOB THE NUMEBER WAS 27") 
+if guess > secret_num : 
+    print("lower") 
+else: 
+    print("higher") 
+    if not guess  == secret_num : 
+            print ("try again")
+            guess = int(input("Guess again : ")) 
+    if guess == secret_num : 
+                print ("GOOD JOB THE NUMEBER WAS 27") 
+    if guess > secret_num : 
+        print("lower") 
+    else: 
+        print("higher") 
+
+guess = int(input("Guess the secret number : "))
+if guess == secret_num : 
+    print ("GOOD JOB THE NUMEBER WAS 27") 
+if guess > secret_num : 
+    print("lower") 
+else: 
+    print("higher") 
+
+    if not guess  == secret_num : 
+        print ("try again")
+        guess = int(input("Guess again : ")) 
+    if guess == secret_num : 
+          print ("GOOD JOB THE NUMEBER WAS 27") 
+if guess > secret_num : 
+    print("lower") 
+else: 
+    print("higher")   
+
+
+
+    
