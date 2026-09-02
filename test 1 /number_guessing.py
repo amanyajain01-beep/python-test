5b95a06fa645bb892193531ccc53bd3b28cb6607
amanyajain01-beep <amanya.jain.01@gmail.com>
2026-09-02 18:15:08 +0530

    second commit

--- test 1 /number_guessing.py
+++ test 1 /number_guessing.py
@@ -39,21 +39,7 @@ else:
     else: 
         print("higher") 
 
-guess = int(input("Guess the secret number : "))
-if guess == secret_num : 
-    print ("GOOD JOB THE NUMEBER WAS 27") 
-if guess > secret_num : 
-    print("lower") 
-else: 
-    print("higher") 
-
-    if not guess  == secret_num : 
-        print ("try again")
-        guess = int(input("Guess again : ")) 
-    if guess == secret_num : 
-          print ("GOOD JOB THE NUMEBER WAS 27") 
-if guess > secret_num : 
-    print("lower") 
-else: 
-    print("higher")   
+if guess < secret_num or  guess > secret_num : 
+        print("So sorry your turns are up the number was 27")
+    
 
